bb934b37e34726588105e921490e39cfe60618d4
Sadat Duraki <sadat.duraki@bil.omu.edu.tr>
2021-09-27 12:09:12 +0200

    Add a dispay of list of cart items

---
+++ shop_app/lib/screens/cart_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart'
+    show
+        Cart; // show import only 'Cart', we use this if there more classes defined in same file
+import '../widgets/cart_item.dart';
+
+class CartScreen extends StatelessWidget {
+  static const String routeName = '/cart';
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Cart'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).primaryTextTheme.headline6.color,
+                      ),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text('ORDER NOW'),
+                    textColor: Theme.of(context).primaryColor,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (ctx, i) {
+                return CartItem(
+                  cart.items.values.toList()[i].id, 
+                  cart.items.values.toList()[i].price,
+                  cart.items.values.toList()[i].quantity, 
+                  cart.items.values.toList()[i].title
+                );
+              },
+              itemCount: cart.itemCount,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
