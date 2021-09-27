250f5d940f04fa08668ea56ca2ed63506edb6bbc
Sadat Duraki <sadat.duraki@bil.omu.edu.tr>
2021-09-27 10:24:49 +0200

    Add multiple providers, Add cart prover

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -4,17 +4,24 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      // 'create:' is used when new object is initiated(ex. Products())
-      create: (ctx) => Products(), 
-      // 'value:' is used when context is not needed and object is already initiated (ex. products[i])
-      // value: Products(), 
+    // 'value:' is used when context is not needed and object is already initiated (ex. products[i])
+    // value: Products(), 
+    // 'create:' is used when new object is initiated(ex. Products())
+    return  MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (ctx) => Products(),
+      ),
+      ChangeNotifierProvider(
+        create: (ctx) => Cart(),
+      ),
+      ], 
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
---
+++ shop_app/lib/providers/cart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.price,
+    @required this.quantity,
+    @required this.title,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItems(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update( 
+          productId,
+          (existingCartItem) => CartItem(
+                id: existingCartItem.id,
+                title: existingCartItem.title,
+                price: existingCartItem.price,
+                quantity: existingCartItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                price: price,
+                quantity: 1,
+              ));
+    }
+  }
+}
