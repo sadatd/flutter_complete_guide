a3cd8df06553401349dbcc5603a6d282c2a353b4
Sadat Duraki <sadat.duraki@bil.omu.edu.tr>
2021-10-05 16:16:56 +0200

    Add changes from Section 11(#261 - #269)
    Add authentication screen(ready code provided by Maximillian).
    Add user signup and user login methods in new Auth provider.
    Add error handlig on login(result shown in dialogBox)
    Manage the auth token locally, so if login is sucessfull, redirect
    from AuthScreen to ProductOveriviewScreen.

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -6,48 +6,52 @@ import './screens/cart_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/orders_screen.dart';
+import './screens/auth_screen.dart';
 import './screens/edit_product_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     // 'value:' is used when context is not needed and object is already initiated (ex. products[i])
     // 'create:' is used when new object is initiated(ex. Products())
-    return  MultiProvider(providers: [
-      ChangeNotifierProvider(
-        create: (ctx) => Products(),
-      ),
-      ChangeNotifierProvider(
-        create: (ctx) => Cart(),
-      ),
-       ChangeNotifierProvider(
-        create: (ctx) => Orders(),
-      ),
-      ], 
-      child: MaterialApp(
-        title: 'MyShop',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-          accentColor: Colors.lightGreen,
-          fontFamily: 'Lato'
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
         ),
-        home: ProductsOverviewScreen(),
-        routes: {
-          CartScreen.routeName: (ctx) => CartScreen(),
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          OrdersScreen.routeName: (ctx) => OrdersScreen(),
-          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-          EditProductScreen.routeName: (ctx) => EditProductScreen(),
-        },
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Orders(),
+        ),
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+              primarySwatch: Colors.green,
+              accentColor: Colors.lightGreen,
+              fontFamily: 'Lato'),
+          home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+          routes: {
+            CartScreen.routeName: (ctx) => CartScreen(),
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            OrdersScreen.routeName: (ctx) => OrdersScreen(),
+            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+            EditProductScreen.routeName: (ctx) => EditProductScreen(),
+          },
+        ),
       ),
     );
   }
 }
-
-
