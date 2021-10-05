5990ba72e180943806625d35a47b39fd875f5819
Sadat Duraki <sadat.duraki@bil.omu.edu.tr>
2021-10-05 10:32:17 +0200

    Add store orders to firebase

--- shop_app/lib/providers/orders.dart
+++ shop_app/lib/providers/orders.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './cart.dart';
 
@@ -23,13 +25,31 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.https(
+        'flutter-sadat-default-rtdb.europe-west1.firebasedatabase.app',
+        '/orders.json');
+    final timestamp = DateTime.now(); 
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts.map((cartProd) => { // products are list of maps
+          'id': cartProd.id,
+          'title': cartProd.title,
+          'quantity': cartProd.quantity,
+          'price': cartProd.price,
+        }).toList(),
+      }),
+    );
+
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
-        dateTime: DateTime.now(),
+        dateTime: timestamp,
         products: cartProducts,
       ),
     );
