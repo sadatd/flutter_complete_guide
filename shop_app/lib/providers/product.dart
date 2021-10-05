50f4f1cf1422b836e38c6e3f9d8f5e88deb2d9bb
Sadat Duraki <sadat.duraki@bil.omu.edu.tr>
2021-10-05 09:58:13 +0200

    Connect isFavorite status with firebase

--- shop_app/lib/providers/product.dart
+++ shop_app/lib/providers/product.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
 
-class Product with ChangeNotifier{
+import 'package:http/http.dart' as http;
+
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
@@ -17,8 +20,33 @@ class Product with ChangeNotifier{
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+  void _setFavValue(bool newValue){
+    isFavorite = newValue;
     notifyListeners();
   }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
+    isFavorite = !isFavorite;
+    notifyListeners();
+    final url = Uri.https(
+      'flutter-sadat-default-rtdb.europe-west1.firebasedatabase.app',
+      '/products/$id.json');
+    try {
+      final response =  await http.patch(
+        url, 
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400){
+        _setFavValue(oldStatus);
+      }
+    } 
+    catch (error) {
+      _setFavValue(oldStatus);
+    }
+    
+  }
+  
 }
