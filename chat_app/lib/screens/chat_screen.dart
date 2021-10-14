0d8123439d3ae54c07ba3bb6bd42942592e5dd76
Sadat Duraki <sadat.duraki@bil.omu.edu.tr>
2021-10-14 11:15:25 +0200

    Fetch data from firebase, and print in console

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (ctx, index) => Container(
-        padding: EdgeInsets.all(8),
-        child: Text('Works'),
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('Works'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/HY2gzVyoxyT54pKIxvXT/messages')
+              .snapshots()
+              .listen((data) {
+            data.documents.forEach((element) {
+              print(element['text']);
+            });
+          });
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
